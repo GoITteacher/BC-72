107618a914bd9a7183bd7b287fa18413145a6f35
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-04-29 10:45:11 +0300

    update

--- js/01-array.js
+++ js/01-array.js
@@ -6,34 +6,92 @@
  * - Колекція значень властивості
  */
 
-const friends = [
-  { name: "Mango", online: false },
-  { name: "Kiwi", online: true },
-  { name: "Poly", online: false },
-  { name: "Ajax", online: false },
-];
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: false },
+//   { name: 'Ajax', online: false },
+// ];
 
-console.table(friends);
+// console.table(friends);
+
+// for (const friend of friends) {
+//   console.log(friend.name);
+// }
+
+// for (const item of friends) {
+//   console.log(item.online);
+// }
 
 /**
  * Пошук друга за іменем
  */
-function findFriendByName(allFriends, friendName) {}
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: false },
+//   { name: 'Ajax', online: false },
+// ];
 
-// console.log(findFriendByName(friends, "Poly"));
+// function findFriendByName(allFriends, friendName) {
+//   for (const friend of allFriends) {
+//     if (friend.name === friendName) {
+//       return friend;
+//     }
+//   }
+// }
+
+// const myFriend = findFriendByName(friends, 'Poly');
+// console.log(myFriend);
+
+// console.log();
 // console.log(findFriendByName(friends, "Chelsy"));
 
 /**
  * Отримуємо імена всіх лрузів
  */
-function getAllNames(allFriends) {}
+
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: false },
+//   { name: 'Ajax', online: false },
+// ];
+
+// function getAllNames(arr) {
+//   const result = []; // ['Mango', 'Kiwi', 'Poly', 'Ajax']
+
+//   for (const item of arr) {
+//     result.push(item.name);
+//   }
+
+//   return result;
+// }
 
 // console.log(getAllNames(friends));
 
 /**
  * Отримуємо імена тільки тих друзів, які зараз онлайн
  */
-function getOnlineFriends(allFriends) {}
+
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: true },
+//   { name: 'Ajax', online: true },
+// ];
+
+// function getOnlineFriends(allFriends) {
+//   const result = [];
+
+//   for (const friend of allFriends) {
+//     if (friend.online) {
+//       result.push(friend.name);
+//     }
+//   }
+
+//   return result;
+// }
 
 // console.log(getOnlineFriends(friends));
 
@@ -44,9 +102,346 @@ function getOnlineFriends(allFriends) {}
  * таким ім'ям, ціною та кількістю з об'єкта
  */
 
-const stones = [
-  { name: "Смарагд", price: 1300, quantity: 4 },
-  { name: "Діамант", price: 2700, quantity: 3 },
-  { name: "Сапфір", price: 400, quantity: 7 },
-  { name: "Щебінь", price: 200, quantity: 2 },
+// const stones = [
+//   { name: 'Смарагд', price: 1300, quantity: 4 },
+//   { name: 'Діамант', price: 2700, quantity: 3 },
+//   { name: 'Сапфір', price: 400, quantity: 7 },
+//   { name: 'Щебінь', price: 200, quantity: 2 },
+// ];
+
+// function calcTotalPrice(stones, stoneName) {
+//   for (const item of stones) {
+//     if (item.name === stoneName) {
+//       // item.totalPrice = item.price * item.quantity;
+//       return item;
+//     }
+//   }
+// }
+
+// console.log(calcTotalPrice(stones, 'Діамант'));
+
+//!===============================================================
+
+const cars = [
+  {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Honda',
+    model: 'Accord',
+    year: 2020,
+    color: 'White',
+    price: 28000,
+    amount: 5,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 2021,
+    color: 'Red',
+    price: 50000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Chevrolet',
+    model: 'Camaro',
+    year: 2018,
+    color: 'Black',
+    price: 45000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'BMW',
+    model: 'M5',
+    year: 2022,
+    color: 'Blue',
+    price: 70000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Mercedes-Benz',
+    model: 'E-Class',
+    year: 2020,
+    color: 'Silver',
+    price: 60000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Audi',
+    model: 'A4',
+    year: 2021,
+    color: 'Gray',
+    price: 45000,
+    amount: 4,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Lamborghini',
+    model: 'Huracan',
+    year: 2023,
+    color: 'Yellow',
+    price: 300000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Ferrari',
+    model: '488 GTB',
+    year: 2022,
+    color: 'Red',
+    price: 350000,
+    amount: 1,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Porsche',
+    model: '911',
+    year: 2021,
+    color: 'White',
+    price: 120000,
+    amount: 2,
+    isSportCar: true,
+    type: 'Coupe',
+  },
+  {
+    brand: 'Toyota',
+    model: 'Golf',
+    year: 2020,
+    color: 'Black',
+    price: 20000,
+    amount: 6,
+    isSportCar: false,
+    type: 'Hatchback',
+  },
+  {
+    brand: 'Subaru',
+    model: 'Forester',
+    year: 2021,
+    color: 'Green',
+    price: 30000,
+    amount: 3,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Jeep',
+    model: 'Wrangler',
+    year: 2022,
+    color: 'Orange',
+    price: 35000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Mazda',
+    model: 'CX-5',
+    year: 2020,
+    color: 'Red',
+    price: 28000,
+    amount: 5,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Kia',
+    model: 'Soul',
+    year: 2021,
+    color: 'Yellow',
+    price: 20000,
+    amount: 4,
+    isSportCar: false,
+    type: 'Hatchback',
+  },
+  {
+    brand: 'Toyota',
+    model: 'Tucson',
+    year: 2022,
+    color: 'White',
+    price: 30000,
+    amount: 3,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Nissan',
+    model: 'Sentra',
+    year: 2019,
+    color: 'Silver',
+    price: 22000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+  },
+  {
+    brand: 'Volvo',
+    model: 'XC60',
+    year: 2020,
+    color: 'Blue',
+    price: 40000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Lexus',
+    model: 'RX',
+    year: 2021,
+    color: 'Black',
+    price: 50000,
+    amount: 2,
+    isSportCar: false,
+    type: 'SUV',
+  },
+  {
+    brand: 'Land Rover',
+    model: 'Range Rover Evoque',
+    year: 2022,
+    color: 'Gray',
+    price: 60000,
+    amount: 1,
+    isSportCar: false,
+    type: 'SUV',
+  },
 ];
+
+/* 
+{
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+}
+
+*/
+
+//!===============================================================
+/* function getCarModels(cars) {
+  const result = [];
+
+  for (const car of cars) {
+    result.push(car.model);
+  }
+
+  return result;
+}
+
+const models = getCarModels(cars);
+
+console.log(models); */
+
+//!===============================================================
+
+/* 
+{
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+}
+
+*/
+
+/* function getModelsByPrice(cars, minPrice) {
+  const result = [];
+  for (const car of cars) {
+    if (car.price > minPrice) {
+      result.push(car.model);
+    }
+  }
+  return result;
+}
+
+const myCars = getModelsByPrice(cars, 50000);
+
+console.log(myCars); */
+
+//!===============================================================
+
+/* 
+{
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+}
+
+*/
+
+/* function task1(cars, color, year) {
+  const result = [];
+
+  for (const car of cars) {
+    if (car.color === color && car.year >= year) {
+      result.push(car);
+    }
+  }
+
+  return result;
+}
+
+console.table(task1(cars, 'White', 2015)); */
+
+//!===============================================================
+
+/* 
+{
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2019,
+    color: 'Silver',
+    price: 25000,
+    amount: 3,
+    isSportCar: false,
+    type: 'Sedan',
+}
+*/
+
+// function getMarkup(cars) {
+//   const result = [];
+
+//   for (const car of cars) {
+//     if (car.color === 'Red') {
+//       result.push(`<li>
+//       <p>Model: ${car.brand} ${car.model}</p>
+//       <p>Price: ${car.price}</p>
+//     </li>`);
+//     }
+//   }
+
+//   return result.join('\n\n\n');
+// }
+
+// console.log(getMarkup(cars));
