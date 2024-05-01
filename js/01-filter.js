c20fc3466dd90b71d41ddce90ab16b5c9a5630e4
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-05-01 11:12:01 +0300

    update

--- js/01-filter.js
+++ js/01-filter.js
@@ -1,3 +1,38 @@
+//!===============================================================
+/* 
+[].map((el, idx, arr)=>{})
+[].filter((el, idx, arr)=>{})
+[].find((el, idx, arr)=>{})
+[].every((el, idx, arr)=>{})
+[].some((el, idx, arr)=>{})
+[].reduce((acc, el, idx ,arr)=>{})
+[].toSorted(()=>{})
+[].sort(()=>{})
+ */
+
+//!===============================================================
+
+/* function getChangeModels(cars) {
+  return cars.map((car, idx, arr) => {
+    const copy = { ...car };
+    copy.model = car.model.toUpperCase();
+    return copy;
+  });
+} */
+
+/* function getMyCars(cars) {
+  return cars.map((car, id, arr) => {
+    const result = {
+      brand: car.brand,
+      model: car.model,
+    };
+
+    return result;
+  });
+}
+console.table(getMyCars(cars)); */
+
+//!===============================================================
 /**
  * Метод filter
  *
@@ -8,91 +43,101 @@
  *    - якщо коллбек повернув false елемент НЕ додається в масив, що повертається
  */
 
-const numbers = [5, 10, 15, 20, 25];
+const numbers = [1, 2, 4, 65, 1, 34, 56, 3, 2, 4, 6, 8, 3, 2, 1, 4, 6, 9, 6];
 
-const filteredNumbers = numbers;
-console.log(filteredNumbers);
+/* const filteredNumbers = numbers.filter((el, idx, arr) => {
+  return el % 2 === 0 && el > 10;
+}); */
+
+// const filteredNumbers = numbers.filter(el => el > 100000);
+// [1, 2, 4, 65, 34, 56]
+// console.log(filteredNumbers);
 
 /**
  * ---------------------------
  */
 const allCars = [
   {
-    make: "Honda",
-    model: "CR-V",
-    type: "suv",
+    id: 1,
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
     amount: 14,
     price: 24045,
     onSale: true,
   },
   {
-    make: "Honda",
-    model: "Accord",
-    type: "sedan",
+    id: 2,
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
     amount: 2,
     price: 22455,
     onSale: true,
   },
   {
-    make: "Mazda",
-    model: "Mazda 6",
-    type: "sedan",
+    id: 3,
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
     amount: 8,
     price: 24195,
     onSale: false,
   },
   {
-    make: "Mazda",
-    model: "CX-9",
-    type: "suv",
+    id: 4,
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
     amount: 7,
     price: 31520,
     onSale: true,
   },
   {
-    make: "Toyota",
-    model: "4Runner",
-    type: "suv",
+    id: 5,
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
     amount: 19,
     price: 34210,
     onSale: false,
   },
   {
-    make: "Toyota",
-    model: "Sequoia",
-    type: "suv",
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
     amount: 16,
     price: 45560,
     onSale: false,
   },
   {
-    make: "Toyota",
-    model: "Tacoma",
-    type: "truck",
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
     amount: 4,
     price: 24320,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "F-150",
-    type: "truck",
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
     amount: 11,
     price: 27110,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "Fusion",
-    type: "sedan",
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
     amount: 13,
     price: 22120,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "Explorer",
-    type: "suv",
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
     amount: 6,
     price: 31660,
     onSale: false,
@@ -104,7 +149,9 @@ const allCars = [
  * значення параметра threshold.
  */
 
-const filterByPrice = (cars, threshold) => {};
+// const filterByPrice = (cars, threshold) => {
+//   return cars.filter(car => car.price < threshold);
+// };
 
 // console.table(filterByPrice(allCars, 30000));
 // console.table(filterByPrice(allCars, 25000));
@@ -114,16 +161,32 @@ const filterByPrice = (cars, threshold) => {};
  * властивість onSale яких true.
  */
 
-const getCarsWithDiscount = (cars) => {};
+// const getCarsWithDiscount = cars => {
+//   return cars.filter(car => car.onSale);
+// };
+
+// const getCarsWithoutDiscount = cars => {
+//   return cars.filter(car => !car.onSale);
+// };
 
 // console.table(getCarsWithDiscount(allCars));
+// console.table(getCarsWithoutDiscount(allCars));
 
 /**
  * Нехай функція getCarsWithType повертає масив автомобілів тип яких збігається
  * зі значенням параметра type.
  */
 
-const getCarsWithType = (cars, type) => {};
+// const getCarsWithType = (cars, type) => {
+//   return cars.filter(car => car.type === type);
+// };
 
-// console.table(getCarsWithType(allCars, "suv"));
-// console.table(getCarsWithType(allCars, "sedan"));
+// console.table(getCarsWithType(allCars, 'suv'));
+// console.table(getCarsWithType(allCars, 'sedan'));
+
+// function removeCarById(cars, id) {
+//   return cars.filter(car => car.id !== id);
+// }
+// console.log(removeCarById(allCars, 5));
+
+//!===============================================================
--- js/03-every-some.js
+++ js/03-every-some.js
@@ -1,9 +1,9 @@
 const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, points: 54, online: true },
-  { id: "player-2", name: "Poly", timePlayed: 470, points: 92, online: false },
-  { id: "player-3", name: "Kiwi", timePlayed: 230, points: 48, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, points: 71, online: false },
-  { id: "player-5", name: "Chelsy", timePlayed: 280, points: 48, online: true },
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: true },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
 ];
 
 /**
@@ -12,7 +12,14 @@ const players = [
  * - Поелементно перебирає оригінальний масив
  * - Повертає true якщо всі елементи масиву задовольняють умову
  */
-const isAllOnline = players;
+
+// const isAllOnline = players.every(user => {
+//   console.log(user.name);
+//   return user.online;
+// });
+
+// true && false && true && false && true = false;
+
 // console.log('isAllOnline: ', isAllOnline);
 
 /**
@@ -21,8 +28,22 @@ const isAllOnline = players;
  * - Поелементно перебирає оригінальний масив
  * - Повертає true якщо хоча б один елемент масиву задовольняє умову
  */
-const isAnyOnline = players;
-console.log("isAnyOnline: ", isAnyOnline);
+// const isAnyOnline = players.some(user => {
+//   return !user.online;
+// });
 
-const anyHardcorePlayers = players;
-console.log("anyHardcorePlayers: ", anyHardcorePlayers);
+// console.log('isAnyOnline: ', isAnyOnline);
+
+// const anyHardcorePlayers = players;
+// console.log('anyHardcorePlayers: ', anyHardcorePlayers);
+
+// const arr = [0, 1, 2, 3, 3, 5, 5, 1, 0, 1, 3, 5];
+
+// while (!arr.every(el => el === 0)) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       arr[i]--;
+//     }
+//   }
+//   console.log(arr.join());
+// }
