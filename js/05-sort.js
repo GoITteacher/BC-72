30b2699c7a08663e5dbbdd2c614ecfcd615c6d89
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-05-02 09:05:07 +0300

    update

--- js/05-sort.js
+++ js/05-sort.js
@@ -9,49 +9,117 @@
  * - сортує за зростанням
  * - приводить елементи до рядка і сортує за [Unicode](https://unicode-table.com/en/)
  */
+//!===============================================================
+// const numbers = [5, 1, 1, 32, 6, 8, 2, 1, 2];
 
-const numbers = [1, 9, 6, 2, 3];
+// const result = numbers.toSorted((b, a) => {
+//   return a - b; -4
+// });
 
-const sorted = numbers;
-console.log("sorted ", sorted);
+// console.log(result);
 
-const letters = ["b", "B", "a", "A"];
-console.log("letters", letters);
+//!===============================================================
+// const numbers = [1, 9, 6, 2, 3];
+
+// const sorted = numbers.toSorted((a, b) => a - b);
+// console.log('sorted ', sorted);
+
+// const letters = ['B', 'a', 'b', 'A'];
+// const sorted = letters.toSorted((a, b) => {
+//   return a.localeCompare(b);
+// });
+// console.log('letters', sorted);
 
 /**
  * compareFunction - функція порівняння (callback)
  * Елементи масиву сортуються відповідно до її значення, що повертається
  */
 
-console.log(
-  numbers.toSorted((curEl, nextEl) => {
-    return nextEl - curEl;
-  })
-);
+// console.log(
+//   numbers.toSorted((curEl, nextEl) => {
+//     return nextEl - curEl;
+//   }),
+// );
 
-const descSortedNumbers = numbers;
-const ascSortedNumbers = numbers;
-console.log("descSortedNumbers", descSortedNumbers);
-console.log("ascSortedNumbers", ascSortedNumbers);
+// const descSortedNumbers = numbers;
+// const ascSortedNumbers = numbers;
+// console.log('descSortedNumbers', descSortedNumbers);
+// console.log('ascSortedNumbers', ascSortedNumbers);
 
 /**
  * Сортування масиву об'єктів
  */
-const players = [
-  { id: "player-1", name: "Mango", timePlayed: 310, online: false },
-  { id: "player-2", name: "Poly", timePlayed: 470, online: true },
-  { id: "player-3", name: "Aiwi", timePlayed: 230, online: true },
-  { id: "player-4", name: "Ajax", timePlayed: 150, online: false },
-  { id: "player-5", name: "Chelsey", timePlayed: 80, online: true },
-];
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, online: true },
+//   { id: 'player-3', name: 'Aiwi', timePlayed: 230, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+//   { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
+// ];
 
 // За ігровим часом
-const sortedByBestPlayers = players;
-console.table(sortedByBestPlayers);
+// const sortedByBestPlayers = players;
+// console.table(sortedByBestPlayers);
 
-const sortedByWorstPlayers = players;
-console.table(sortedByWorstPlayers);
+// const sortedByWorstPlayers = players;
+// console.table(sortedByWorstPlayers);
 
-// По первой букве имени
-const byName = players;
-console.table(byName);
+// // По первой букве имени
+// const byName = players;
+// console.table(byName);
+
+//!===============================================================
+
+// console.log(cars);
+
+// const sorted = cars.toSorted((b, a) => {
+//   return a.color.localeCompare(b.color);
+// });
+
+// console.table(sorted);
+
+//!===============================================================
+
+// const sorted = cars.toSorted((b, a) => {
+//   return a.color.localeCompare(b.color);
+// });
+
+// const sorted = cars.toSorted((b, a) => {
+//   return a.price - b.price;
+// });
+
+// const sorted = cars.toSorted((b, a) => {
+//   return a.isSportCar - b.isSportCar;
+// });
+
+// const sorted = cars.toSorted((a, b) => {
+//   return a.amount - b.amount;
+// });
+
+// let count = 0;
+// const sorted = cars.toSorted((a, b) => {
+//   count++;
+//   if (a.isSportCar === b.isSportCar) {
+//     if (a.color === b.color) {
+//       return a.color.localeCompare(b.color);
+//     } else {
+//       return a.model.localeCompare(b.model);
+
+//     }
+//   }
+//   return a.isSportCar - b.isSportCar;
+// });
+
+// const sorted = cars
+//   .toSorted((a, b) => {
+//     return a.color.localeCompare(b.color);
+//   })
+//   .toSorted((a, b) => {
+//     return a.model.localeCompare(b.model);
+//   })
+//   .toSorted((a, b) => {
+//     return a.isSportCar - b.isSportCar;
+//   });
+
+// console.log(count);
+// console.table(sorted);
--- js/06-chaining.js
+++ js/06-chaining.js
@@ -1,100 +1,106 @@
 /**
  * Ланцюжки методів
  */
-const numbers = [1, 5, 2, 4, 3];
 
-const greaterThenTwo = numbers;
-console.log(greaterThenTwo);
+// const numbers = [1, 5, 2, 4, 3];
 
-const multByThree = greaterThenTwo;
-console.log(multByThree);
+// const greaterThenTwo = numbers.filter(el => el > 2);
+// console.log(greaterThenTwo);
 
-const sorted = multByThree;
-console.log(sorted);
+// const multByThree = greaterThenTwo.map(el => el * 3);
+// console.log(multByThree);
 
-const sortedbyChaining = numbers;
-console.log(sortedbyChaining);
+// const sorted = multByThree.toSorted((a, b) => a - b);
+
+// console.log(sorted);
+
+// const sortedbyChaining = numbers
+//   .filter(el => el > 2)
+//   .map(el => el * 3)
+//   .toSorted((a, b) => a - b);
+
+// console.log(sortedbyChaining);
 
 /**
  * -------------------------------------
  */
 const allCars = [
   {
-    make: "Honda",
-    model: "CR-V",
-    type: "suv",
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
     amount: 14,
     price: 24045,
     onSale: true,
   },
   {
-    make: "Honda",
-    model: "Accord",
-    type: "sedan",
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
     amount: 2,
     price: 22455,
     onSale: true,
   },
   {
-    make: "Mazda",
-    model: "Mazda 6",
-    type: "sedan",
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
     amount: 8,
     price: 24195,
     onSale: false,
   },
   {
-    make: "Mazda",
-    model: "CX-9",
-    type: "suv",
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
     amount: 7,
     price: 31520,
     onSale: true,
   },
   {
-    make: "Toyota",
-    model: "4Runner",
-    type: "suv",
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
     amount: 19,
     price: 34210,
     onSale: false,
   },
   {
-    make: "Toyota",
-    model: "Sequoia",
-    type: "suv",
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
     amount: 16,
     price: 45560,
     onSale: false,
   },
   {
-    make: "Toyota",
-    model: "Tacoma",
-    type: "truck",
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
     amount: 4,
     price: 24320,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "F-150",
-    type: "truck",
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
     amount: 11,
     price: 27110,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "Fusion",
-    type: "sedan",
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
     amount: 13,
     price: 22120,
     onSale: true,
   },
   {
-    make: "Ford",
-    model: "Explorer",
-    type: "suv",
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
     amount: 6,
     price: 31660,
     onSale: false,
@@ -105,14 +111,73 @@ const allCars = [
  * Нехай функція getAvailableCarNames повертає масив моделей автомобілів,
  * але тільки тих, які зараз на розпродажі.
  */
-const getModelsOnSale = (cars) => {};
-
-// console.table(getModelsOnSale(allCars));
+// const getModelsOnSale = cars => {
+//   return cars.filter(car => car.onSale).map(car => car.model);
+// };
+// console.log(getModelsOnSale(allCars));
 
 /**
  * Нехай функція getSortedCarsOnSale повертає масив автомобілів
  * на розпродажі (Властивість onSale), відсортованих за зростанням ціни.
  */
-const getSortedCarsOnSale = (cars) => {};
-
+// const getSortedCarsOnSale = cars => {
+//   return cars.filter(car => car.onSale).toSorted((a, b) => a.price - b.price);
+// };
 // console.table(getSortedCarsOnSale(allCars));
+
+// =================================
+
+// const numbers = [1, 3, 5, 2, 3, 4, 1, 2, 34, 6, 7, 2, 1, 1];
+
+// const result = numbers
+//   .filter(el => el % 2 === 0)
+//   .map((el, idx, arr) => el * arr.length);
+
+// console.log(numbers);
+// console.log(result);
+
+//!===============================================================
+
+/* 
+{
+    firstName: 'Alice',
+    lastName: 'Johnson',
+    age: 28,
+    email: 'alice@example.com',
+    isActive: true,
+    gender: 'Female',
+},
+*/
+
+// 1. Знайти користувачів віком від 25 до 40 років.
+
+// const result1 = users.filter(user => user.age >= 30 && user.age < 35);
+// console.table(result1);
+
+// 1. Знайти всіх користувачів із активним статусом.
+
+// const result2 = users.filter(user => user.isActive);
+// const result3 = users.filter(user => !user.isActive);
+// console.table(result3);
+
+// 1. Порахувати кількість користувачів молодше 30 років.
+// const result4 = users.filter(user => user.age < 30).length;
+// console.log(result4);
+
+// 1. Відсортувати користувачів віком від старшого до молодшого.
+
+// const result5 = users.toSorted((b, a) => {
+//   return a.age - b.age;
+// });
+
+// console.table(result5);
+
+// 1. Знайти користувачів з електронною поштою на домені gmail.com.
+
+// const result6 = users
+//   .filter(user => user.email.endsWith('@gmail.com'))
+//   .map(user => `${user.firstName} ${user.lastName}`);
+
+// console.log(result6);
+// 1. Знайти всіх жінок серед користувачів.
+// 1. Порахувати кількість неактивних користувачів.
