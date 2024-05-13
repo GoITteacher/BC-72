7067d0f3803149371c4ebcbc86f39943a1787466
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-05-13 09:19:56 +0300

    update

---
+++ src/library/data.js
@@ -0,0 +1,28 @@
+const images = [
+  {
+    imgUrl:
+      'https://play-lh.googleusercontent.com/rfWOJQVBHoAZ_B43v0ySFlLmJBLtksVGAxGaFRh2ex4nOmNQ86qzG4sYWV63IKrXlvI',
+    title: 'Test',
+  },
+  {
+    imgUrl:
+      'https://habrastorage.org/webt/qd/ex/p9/qdexp9td-qnebatzzddqp60y7qi.jpeg',
+    title: 'Test',
+  },
+  {
+    imgUrl:
+      'https://timeweb.com/ru/community/article/5d/5dea5ac04cd2c5392c0528ad075dca69.jpg',
+    title: 'Test',
+  },
+  {
+    imgUrl:
+      'https://habrastorage.org/webt/uf/x1/ms/ufx1msnzl3xgtcmlt-rkqhpytma.png',
+    title: 'Test',
+  },
+  { imgUrl: 'https://i.ytimg.com/vi/fHl7UyRjOf0/hqdefault.jpg', title: 'Test' },
+  {
+    imgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNuFkbbi7DdadE5zKIHt5vLtNOFhQbBTNhyX05snpcsg&s',
+    title: 'Test',
+  },
+];
